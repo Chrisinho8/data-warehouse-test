4eff4217eca584194fef99d430833120a54a4d0f
Chrisinho8 <chrisse.ruck@gmail.com>
2026-05-25 15:48:36 +0200

    Add files via upload

---
+++ scripts/Initialize_database1.sql
@@ -0,0 +1,5 @@
+-- Warning: Executing the following queries will delete the database called 'datawarehouse'.
+-- Executing it, will als create a new database, which this project will take place in
+
+DROP DATABASE IF EXISTS datawarehouse;
+CREATE DATABASE datawarehouse;
---
+++ scripts/Initialize_database2.sql
@@ -0,0 +1,5 @@
+-- Create the 3 schemas that we will be working in and creating our datawarehouse in
+
+CREATE SCHEMA bronze;
+CREATE SCHEMA silver;
+CREATE SCHEMA gold;
